14fd752d31e5acc2ef6abc710f54655abe93bee7
jarenworme <jarenworme@gmail.com>
2025-05-21 10:20:20 -0400

    small change to test auto deployment on pr merge

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Bariffle Group
+          Auto Deployment Test
         </a>
       </header>
     </div>
